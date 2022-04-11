ab3b56876006e625153fa604176239a5c8e5c79a
Yasemin <ysmnyolcu@gmail.com>
2022-04-12 00:48:30 +0200

    day38: Inheritance Continue- Overriding-Tesla

--- JavaProgramming/src/day38_Inheritance/carTask/BMW.java
+++ JavaProgramming/src/day38_Inheritance/carTask/BMW.java
@@ -3,14 +3,14 @@ package day38_Inheritance.carTask;
 public class BMW extends Car {
 
     public BMW(String brand, String model, int year, double price, String color, double miles) {
-        super(brand, model, year, price, color, miles);
+        super("BMW", model, year, price, color, miles);
     }
 
     public void breaksDown(){
-        System.out.println();
+        System.out.println(brand+" "+model+" breaks down every 100 miles");
     }
 
     public void racing(){
-        System.out.println();
+        System.out.println(brand+" "+model+" is a racing car");
     }
 }
--- JavaProgramming/src/day38_Inheritance/carTask/Tesla.java
+++ JavaProgramming/src/day38_Inheritance/carTask/Tesla.java
@@ -6,10 +6,10 @@ public class Tesla extends Car {
 
 
     public Tesla(String brand, String model, int year, double price, String color, double miles) {
-        super(brand, model, year, price, color, miles);
+        super("Tesla", model, year, price, color, miles);
     }
 
     public void autoPilot(){
-        System.out.println();
+        System.out.println(brand+" "+model+" in autopilot mode");
     }
 }
